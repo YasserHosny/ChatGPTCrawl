05d6fd8a86ac4bf631cf19f90687de8f4107715c
Yasser Hosny <yasser_hosny@siliconexpert.com>
2024-05-29 16:48:56 +0300

    Get OpenAI key from OS env

--- chatgptapi/test_chatgpt_api.py
+++ chatgptapi/test_chatgpt_api.py
@@ -2,16 +2,16 @@ from openai import OpenAI
 import os
 import base64
 
+
 class ChatgptApiTest:
     def image_b64(self, image):
         with open(image, "rb") as f:
             return base64.b64encode(f.read()).decode()
 
+    def testChatgptApi(self, user_input, image_file):
+        b64_image = base64.b64encode(image_file).decode("utf-8")
 
-    def testChatgptApi(self, user_input, image_file):   
-        b64_image = base64.b64encode(image_file).decode('utf-8')
-
-        model = OpenAI(        )
+        model = OpenAI(api_key=os.getenv("api_key"))
         print("client.api_key:" + model.api_key)
         response = model.chat.completions.create(
             model="gpt-4-vision-preview",
@@ -30,4 +30,3 @@ class ChatgptApiTest:
         )
         print({"response": response.choices[0].message.content})
         return response.choices[0].message.content
-
